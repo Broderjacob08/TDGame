946f8f64515b00a69a8999e309457efbff1c8285
FedericoQB <federico.quijano@outlook.com>
2025-06-02 11:07:36 +0200

    Fixed UI Health

--- TD game/Assets/FedericoStuff/EnemyAI/EnemyHealth.cs
+++ TD game/Assets/FedericoStuff/EnemyAI/EnemyHealth.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public float health;
+    [SerializeField] private Image Healthbar;
 
     void Update()
     {
+        Healthbar.fillAmount -= health / 100;
+
         if (health <= 0)
         {
             Debug.Log("Game Over");
